a72817ee83ecd618152e1ca642d2716baec40797
kpxp <gk19839601@qq.com>
2015-10-18 16:24:35 +0800

    新增事件效果类型230,235

---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect230.cs
@@ -0,0 +1,29 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+    internal class EventEffect230 : EventEffectKind
+    {
+        private int increment;
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            if (person.BelongedFactionWithPrincess != null)
+            {
+                person.AdjustRelation(person.BelongedFactionWithPrincess.Leader, 0, increment);
+            }
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.increment = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect235.cs
@@ -0,0 +1,29 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+    internal class EventEffect235 : EventEffectKind
+    {
+        private int increment;
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            if (person.BelongedFactionWithPrincess != null)
+            {
+                person.BelongedFactionWithPrincess.Leader.AdjustRelation(person, 0, increment);
+            }
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.increment = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
