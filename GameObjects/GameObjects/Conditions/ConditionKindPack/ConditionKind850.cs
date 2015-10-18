ba8ea072b0070c822dd49572816772b406a1fced
kpxp <gk19839601@qq.com>
2015-10-18 16:32:38 +0800

    新增条件类型850 - 865

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind850.cs
@@ -0,0 +1,28 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+    using System.Collections.Generic;
+
+    internal class ConditionKind850 : ConditionKind
+    {
+        private int number = 0;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            return person.BelongedFactionWithPrincess != null && person.GetRelation(person.BelongedFactionWithPrincess.Leader) >= this.number;
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.number = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
