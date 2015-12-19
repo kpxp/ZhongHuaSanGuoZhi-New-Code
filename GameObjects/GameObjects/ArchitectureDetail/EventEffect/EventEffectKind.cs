e781edfff6834a7cd06cc31b9b4765ea94048979
kpxp <gk19839601@qq.com>
2015-12-19 15:28:43 +0800

    新增事件效果250：武将在某势力仕官

--- GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKind.cs
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKind.cs
@@ -8,7 +8,11 @@
         public virtual void ApplyEffectKind(Person person, Event e)
         {
         }
-
+        /*
+        public virtual void ApplyEffectKind(FactionList factions,Person person, Event e)
+        {
+        }
+        */
         public virtual void ApplyEffectKind(Architecture architecture, Event e)
         {
             foreach (Person p in architecture.Persons)
@@ -36,6 +40,11 @@
         public virtual void InitializeParameter2(string parameter2)
         {
         }
+        /*
+        public virtual void InitializeParameter3(FactionList factions, string parameter)
+        {
+        }
+        */
     }
 }
 
---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect250.cs
@@ -0,0 +1,54 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+    internal class EventEffect250 : EventEffectKind
+    {
+        private int  targetFactionID;
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.targetFactionID = int.Parse(parameter);
+                //targetFaction = factions.GetGameObject(int.Parse(parameter)) as Faction;
+            }
+            catch
+            {
+                
+            }
+           
+        }
+
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            FactionList factionlist = person.Scenario.Factions;
+            Faction targetFaction = factionlist.GetGameObject(targetFactionID) as Faction;
+            /*
+            if (targetFaction != null)
+            {
+                throw new Exception("targetFaction 为" + targetFaction.Name);
+            }
+            */
+
+            if (person.BelongedFaction == null && person.LocationArchitecture != null && targetFaction != null)
+            {
+                //person.Status = GameObjects.PersonDetail.PersonStatus.Normal;
+                person.MoveToArchitecture(targetFaction.Capital);
+                person.ChangeFaction(targetFaction);
+            }
+            else if (person.LocationArchitecture != null && person.LocationArchitecture.BelongedFaction != null)
+            {
+                person.MoveToArchitecture(targetFaction.Capital);
+                person.ChangeFaction(targetFaction);
+                //person.ChangeFaction(targetFaction);
+            }
+        }
+
+       
+
+    }
+}
+
