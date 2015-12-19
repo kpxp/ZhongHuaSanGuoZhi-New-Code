7a38655611edb110c0a2db5e5d4c7730ce1178c0
kpxp <gk19839601@qq.com>
2015-12-19 17:10:48 +0800

    新增势力合并事件效果280

---
+++ GameObjects/GameObjects/ArchitectureDetail/EventEffect/EventEffectKindPack/EventEffect280.cs
@@ -0,0 +1,42 @@
+﻿namespace GameObjects.ArchitectureDetail.EventEffect
+{
+    using GameObjects;
+    using System;
+
+
+    internal class EventEffect280: EventEffectKind
+    {
+        private int  mergeFactionID;
+        
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.mergeFactionID = int.Parse(parameter);
+                
+            }
+            catch
+            {
+                
+            }
+           
+        }
+
+        public override void ApplyEffectKind(Person person, Event e)
+        {
+            FactionList factionlist = person.Scenario.Factions;
+            Faction oldFaction = person .BelongedFaction ;
+            Faction mergeFaction = factionlist.GetGameObject(mergeFactionID) as Faction;
+
+            if (oldFaction != null && mergeFaction != null && person == oldFaction.Leader)
+            {
+                oldFaction.ChangeFaction(mergeFaction);
+            }
+           
+        }
+
+
+
+    }
+}
+
