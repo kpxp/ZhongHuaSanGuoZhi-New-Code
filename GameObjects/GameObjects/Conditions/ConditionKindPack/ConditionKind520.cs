068dbaae077564bc8e1e9bed2d389e8bbdeca08b
kpxp <gk19839601@qq.com>
2015-12-25 18:38:26 +0800

    新增武将同城、同势力条件类型

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind520.cs
@@ -0,0 +1,28 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind520 : ConditionKind
+    {
+        private int personID;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            Person p1 = person.Scenario.Persons.GetGameObject(personID) as Person;
+            return ( person.LocationArchitecture != null && p1.LocationArchitecture  != null && person.LocationArchitecture == p1.LocationArchitecture );
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.personID = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind530.cs
@@ -0,0 +1,28 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind530 : ConditionKind
+    {
+        private int personID;
+
+        public override bool CheckConditionKind(Person person)
+        {
+            Person p1 = person.Scenario.Persons.GetGameObject(personID) as Person;
+            return (person.BelongedFaction != null && p1.BelongedFaction != null && person.BelongedFaction == p1.BelongedFaction);
+        }
+
+        public override void InitializeParameter(string parameter)
+        {
+            try
+            {
+                this.personID = int.Parse(parameter);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
