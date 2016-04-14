59e63551a45c9372d09c78071e1acf99321db40d
kpxp <gk19839601@qq.com>
2016-04-15 01:36:36 +0800

    新增任意武将同城条件类型540

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind540.cs
@@ -0,0 +1,34 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind540 : ConditionKind
+    {
+        
+        public override bool CheckConditionKind(Person person)
+        {
+             person.ID  = -1;
+            
+               if(person .ID == -1)
+               {
+                   person = person.Scenario.Persons[GameObject.Random(person.Scenario.Persons.Count)] as Person;
+               }
+
+
+               if (person.LocationArchitecture != null)
+               {
+                   Person p1 = person.LocationArchitecture.Persons[GameObject.Random(person.LocationArchitecture.Persons.Count)] as Person;
+
+                   if (person.LocationArchitecture == p1.LocationArchitecture)
+                   {
+                       return true;
+                   }
+               }
+               return false;
+        }
+
+    }
+}
+
