d291b99de4774e709e2810bc02a1ea5dd88a21a2
kpxp <gk19839601@qq.com>
2016-04-15 15:02:07 +0800

    新增条件类型任意武将所在势力690

---
+++ GameObjects/GameObjects/Conditions/ConditionKindPack/ConditionKind690.cs
@@ -0,0 +1,32 @@
+﻿namespace GameObjects.Conditions.ConditionKindPack
+{
+    using GameObjects;
+    using GameObjects.Conditions;
+    using System;
+
+    internal class ConditionKind690 : ConditionKind
+    {
+        
+        public override bool CheckConditionKind(Person person)
+        {
+            if (person.ID == -1)
+            {
+                person = person.Scenario.Persons[GameObject.Random(person.Scenario.Persons.Count)] as Person;
+            }
+
+
+            if (person.LocationArchitecture != null)
+            {
+                Faction faction = person.LocationArchitecture.BelongedFaction as Faction;
+
+                if (person.BelongedFaction == faction )
+                {
+                    return true;
+                }
+            }
+            return false; ;
+        }
+
+        
+    }
+}
